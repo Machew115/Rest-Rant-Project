176ef14291eafc4371f6897cdc81ea2e852bf9f4
Matthew Herrera <why.mherrera@gmail.com>
2022-08-04 17:47:44 -0700

    404 error page

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
 
-app.get('/', function(req, res){
+app.get('/', (req, res)=>{
     res.send("hello world")
 })
 
-app.listen(3001)
+app.get('*', (req, res) => {
+    res.status(404).send("<h1>404 Page Not Found</h1>")
+})
+
+app.listen(process.env.PORT)
