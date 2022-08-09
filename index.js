be913a3e163fca0c2dd2f4e756ab0f4be2b0a581
Matthew Herrera <why.mherrera@gmail.com>
2022-08-08 18:49:47 -0700

    bootstrap and basic css added

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+// modules and globals
 require('dotenv').config()
 const express = require('express')
 const app = express()
 
+// express settings
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+//controllers
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res)=>{
