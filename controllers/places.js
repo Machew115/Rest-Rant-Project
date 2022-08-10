f7fc249ca606eae2541a9708f837f3ea14b4cc6b
Matthew Herrera <why.mherrera@gmail.com>
2022-08-10 14:42:39 -0700

    new places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,9 @@
 const router = require('express').Router()
-  
+const places = require('../models/places.js'
+)
+router.get('/', (req, res) => {
+    res.render('places/index', { places })
+})
 
 router.get('/', (req, res) => {
     let places = [{
@@ -19,12 +23,21 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-    console.log(req.body)
-    res.send('POST /places')
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
   })
   
-
-router.get('/new', (req, res) => {
+  router.get('/new', (req, res) => {
     res.render('places/new')
 })
 
