9af67a7503edb5bdc76a55d8101e0a0bb54322b0
Matthew Herrera <why.mherrera@gmail.com>
2022-08-10 14:27:55 -0700

    form data is getting through

--- views/default.jsx
+++ views/default.jsx
@@ -5,7 +5,7 @@ function Def (html) {
    <html>
         <head>
             <title>Title</title>
-            <link rel="stylesheets" href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js'/>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossOrigin="anonymous"/>
             <link rel="stylesheet" href="/css/style.css"/>
         </head>
         <body>
