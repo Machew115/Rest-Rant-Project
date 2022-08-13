0dfa507dc32f86e328430c41157fb6d60554ac06
Matthew Herrera <why.mherrera@gmail.com>
2022-08-13 12:34:29 -0700

    fixed edit functionality

--- controllers/places.js
+++ controllers/places.js
@@ -38,6 +38,7 @@ router.get('/:id', (req, res) => {
 })
 
 router.put('/:id', (req, res) => {
+  
   let id = Number(req.params.id)
   if (isNaN(id)) {
       res.render('error404')
@@ -87,7 +88,7 @@ router.get('/:id/edit', (req, res) => {
       res.render('error404')
   }
   else {
-    res.render('places/edit', { place: places[id] })
+    res.render('places/edit', { place: places[id], id })
   }
 })
 
