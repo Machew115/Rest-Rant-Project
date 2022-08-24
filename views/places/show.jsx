cc4e90664b3d5bb25e5b4266a83c58c0c431cb90
Matthew Herrera <why.mherrera@gmail.com>
2022-08-23 20:37:23 -0700

    all done

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -8,6 +8,24 @@ function show (data) {
       No comments yet!
     </h3>
   )
+  let rating = (
+    <h3 className='inactive'>Not yet rated</h3>
+  )
+  if (data.place.comments.length) {
+    let sumRatings = data.place.comments.reduce((tot, c) => {
+      return tot + c.stars
+    }, 0)
+    let averageRating = Math.round(sumRatings / data.place.comments.length)
+      let stars = ''
+      for (let i = 0; i < averageRating; i++) {
+        stars += '⭐️'
+      }
+    rating = (
+      <h3>
+       {stars} stars
+      </h3>
+    )
+  }
   if (data.place.comments.length) {
     comments = data.place.comments.map(c => {
       return (
@@ -37,10 +55,8 @@ function show (data) {
             <h2>
               Rating
             </h2>
-            <h3 className="inactive">
-              Not Rated yet
-            </h3>
-            <br />
+            {rating}
+            <br/>
             <h2>
               Description
             </h2>
@@ -51,10 +67,10 @@ function show (data) {
               Serving {data.place.cuisines}
             </h4>
             <br />
-            <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+            <a href={`/places/${data.place.id}/edit`} className="btn btn-warning">
               Edit
             </a>{` `}
-            <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+            <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}>
               <button type="submit" className="btn btn-danger">
                 Delete
               </button>
